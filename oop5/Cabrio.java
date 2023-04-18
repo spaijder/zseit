ed9c1e5ffa118f9a0407c9ec253b8ff4f3607652
spaijder <72754663+spaijder@users.noreply.github.com>
2023-04-18 10:16:28 +0200

    Update Cabrio.java

--- oop5/Cabrio.java
+++ oop5/Cabrio.java
@@ -1,77 +1,114 @@
+/**
+
+A class representing a Cabrio car with a name, brand, roof state and move.
+*/
 package org.example;
 public class Cabrio {
-    private String name;
-    private String brand;
-    private boolean isMoving;
-    private boolean isRoofOpen;
+    private String name; // name of the car
+    private String brand; // brand of the car
+    private boolean isMoving; // current moving state of the car
+    private boolean isRoofOpen; // current state of the roof
+    /**
+ * Constructs a new Cabrio car with the given name and brand.
+ * @param name the name of the car
+ * @param brand the brand of the car
+ */
+public Cabrio(String name, String brand) {
+    this.name = name;
+    this.brand = brand;
+    this.isMoving = false;
+    this.isRoofOpen = false;
+}
 
-    public Cabrio(String name, String brand) {
-        this.name = name;
-        this.brand = brand;
-        this.isMoving = false;
-        this.isRoofOpen = false;
+/**
+ * Returns the name of the car.
+ * @return the name of the car
+ */
+public String getName() {
+    return name;
+}
+
+/**
+ * Returns the brand of the car.
+ * @return the brand of the car
+ */
+public String getBrand() {
+    return brand;
+}
+
+/**
+ * Returns the current moving state of the car.
+ * @return true if the car is moving, false otherwise
+ */
+public boolean isMoving() {
+    return isMoving;
+}
+
+/**
+ * Returns the current state of the roof.
+ * @return true if the roof is open, false otherwise
+ */
+public boolean isRoofOpen() {
+    return isRoofOpen;
+}
+
+/**
+ * Starts the car's movement.
+ */
+public void startMoving() {
+    this.isMoving = true;
+}
+
+/**
+ * Stops the car's movement.
+ */
+public void stopMoving() {
+    this.isMoving = false;
+}
+
+/**
+ * Opens the car's roof.
+ * Cannot be done while the car is in motion.
+ */
+public void openRoof() {
+    if (isMoving) {
+        System.out.println("Error: Cannot open roof while car is in motion.");
+        return;
     }
+    this.isRoofOpen = true;
+}
 
-    public String getName() {
-        return name;
+/**
+ * Closes the car's roof.
+ * Cannot be done while the car is in motion.
+ */
+public void closeRoof() {
+    if (isMoving) {
+        System.out.println("Error: Cannot close roof while car is in motion.");
+        return;
     }
+    this.isRoofOpen = false;
+}
 
-    public String getBrand() {
-        return brand;
-    }
+/**
+ * The main method used to test the Cabrio class.
+ * Creates a new Cabrio car, opens and closes the roof, starts and stops the car's movement.
+ * @param args command-line arguments (not used)
+ */
+public static void main(String[] args) {
+    Cabrio car = new Cabrio("MX-5", "Mazda");
+    System.out.println("Initial state: Moving: " + car.isMoving() + ", Roof Open: " + car.isRoofOpen());
 
-    public boolean isMoving() {
-        return isMoving;
-    }
+    car.openRoof();
+    System.out.println("Roof Opened: Moving: " + car.isMoving() + ", Roof Open: " + car.isRoofOpen());
 
-    public boolean isRoofOpen() {
-        return isRoofOpen;
-    }
+    car.stopMoving();
+    System.out.println("Car stopped moving: Moving: " + car.isMoving() + ", Roof Open: " + car.isRoofOpen());
 
-    public void startMoving() {
-        this.isMoving = true;
-    }
+    car.openRoof();
+    System.out.println("Roof Opened: Moving: " + car.isMoving() + ", Roof Open: " + car.isRoofOpen());
 
-    public void stopMoving() {
-        this.isMoving = false;
-    }
-
-    public void openRoof() {
-        if (isMoving) {
-            System.out.println("Error: Cannot open roof while car is in motion.");
-            return;
-        }
-        this.isRoofOpen = true;
-    }
-
-    public void closeRoof() {
-        if (isMoving) {
-            System.out.println("Error: Cannot close roof while car is in motion.");
-            return;
-        }
-        this.isRoofOpen = false;
-    }
-
-    public static void main(String[] args) {
-        Cabrio car = new Cabrio("MX-5", "Mazda");
-        System.out.println("Initial state: Moving: " + car.isMoving() + ", Roof Open: " + car.isRoofOpen());
-
-        car.openRoof();
-        System.out.println("Roof Opened: Moving: " + car.isMoving() + ", Roof Open: " + car.isRoofOpen());
-
-        car.startMoving();
-        System.out.println("Car started moving: Moving: " + car.isMoving() + ", Roof Open: " + car.isRoofOpen());
-
-        car.closeRoof();
-        System.out.println("Trying to close roof while moving: Moving: " + car.isMoving() + ", Roof Open: " + car.isRoofOpen());
-
-        car.stopMoving();
-        System.out.println("Car stopped moving: Moving: " + car.isMoving() + ", Roof Open: " + car.isRoofOpen());
-
-        car.openRoof();
-        System.out.println("Roof Opened: Moving: " + car.isMoving() + ", Roof Open: " + car.isRoofOpen());
-
-        car.closeRoof();
-        System.out.println("Roof closed: Moving: " + car.isMoving() + ", Roof Open: " + car.isRoofOpen());
+    car.closeRoof();
+    System.out.println("Roof closed: Moving: " + car.isMoving() + ", Roof Open: " + car.isRoofOpen());
     }
 }
